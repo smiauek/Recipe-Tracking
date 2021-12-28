45a67a749de932ab23e15a030bf4fa7d76ba4005
smiauek <92834320+smiauek@users.noreply.github.com>
2021-12-28 14:26:13 -0600

    Add files via upload

---
+++ src/RecipeCreate.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+function RecipeCreate(props) {
+  const initialFormState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormState });
+
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.setRecipes([...props.recipes, formData]);
+    setFormData({ ...initialFormState });
+  };
+
+  return (
+    <form name="create" onSubmit={handleSubmit}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                style={{ width: "100%" }}
+                onChange={handleChange}
+                value={formData.name}
+              />
+            </td>
+            <td>
+              <input
+                name="cuisine"
+                type="text"
+                placeholder="Cuisine"
+                style={{ width: "100%" }}
+                onChange={handleChange}
+                value={formData.cuisine}
+              />
+            </td>
+            <td>
+              <input
+                name="photo"
+                type="url"
+                placeholder="URL"
+                style={{ width: "100%" }}
+                onChange={handleChange}
+                value={formData.photo}
+              />
+            </td>
+            <td>
+              <textarea
+                name="ingredients"
+                type="text"
+                placeholder="Ingredients"
+                style={{ width: "100%" }}
+                onChange={handleChange}
+                value={formData.ingredients}
+              />
+            </td>
+            <td>
+              <textarea
+                name="preparation"
+                type="text"
+                placeholder="Preparation"
+                style={{ width: "100%" }}
+                onChange={handleChange}
+                value={formData.preparation}
+              />
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+export default RecipeCreate;
